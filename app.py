c1f35301497d07458cb42b1a8ea0c33101a5e9ce
Christian Vazquez <ChristianVazquez1@usf.edu>
2025-11-23 21:21:43 -0500

    Add project folder and initial app.py with welcome message

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-#welcome message
 print("Welcome to my Python program!")
-# inputs number of hours into formula
-hours = input("How many hours did you study today? ")
-# converts into float
-hours = float(hours)
-weekly_hours = hours * 7
-#prevents errors from invalid input
-try:
-    hours = float(hours)
-except ValueError:
-    print("Please enter a valid number.")
-    exit()
