7324b76c65c9ca9838a9f8cfb7619b9fb0ae98df
Christian Vazquez <ChristianVazquez1@usf.edu>
2025-11-23 21:28:44 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+#welcome message
 print("Welcome to my Python program!")
+#collect input
 hours = input("How many hours did you study today? ")
+#convert hours to float and give weekly study pace
 hours = float(hours)
 weekly_hours = hours * 7
+#clean output
 print(f"You are on track to study {weekly_hours} hours this week.")
+#prevent error from invalid input
 try:
     hours = float(hours)
 except ValueError:
